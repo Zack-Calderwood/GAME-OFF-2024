a230b54cb7640a69bd8497f2f9f77bd4ae75cb45
Eriki Enos <Erikjenos@hotmail.com>
2024-11-03 23:41:34 -0500

    spotlight Rotate

---
+++ game-off-2024/Scenes/Flashlight.gd
@@ -0,0 +1,16 @@
+extends PointLight2D
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	var mouse_Pos = get_global_mouse_position()
+	
+	var dire = mouse_Pos - global_position
+	
+	rotation = dire.angle()
+	
+	pass
