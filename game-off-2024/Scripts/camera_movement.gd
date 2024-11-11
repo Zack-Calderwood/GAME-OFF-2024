5b6ec5290ddc9e7e84d182b674a5fe67dde27b01
Eriki Enos <Erikjenos@hotmail.com>
2024-11-11 01:33:10 -0500

    camera shake

--- game-off-2024/Scripts/camera_movement.gd
+++ game-off-2024/Scripts/camera_movement.gd
@@ -6,9 +6,19 @@ var follow_speed = 1.75
 
 var mouse_offset_distance = 50.0
 
+
+@export var randomStrength: float = 1
+@export var shakeFade: float = 5.0
+
+var shake_strength: float = 0.0
+
+var rng = RandomNumberGenerator.new()
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	camera_movement(delta)
+	#apply_shake()
 	pass
 	
 func camera_movement(delta: float) -> void:
@@ -21,3 +31,17 @@ func camera_movement(delta: float) -> void:
 	target_position += direction_to_mouse * mouse_offset_distance
 
 	global_position = global_position.lerp(target_position, follow_speed * delta)
+	
+	camera_shake(delta)
+
+func apply_shake() -> void : 
+	shake_strength = randomStrength
+	
+func randomOffset() -> Vector2:
+	return Vector2(rng.randf_range(-shake_strength, shake_strength),rng.randf_range(-shake_strength, shake_strength))
+
+func camera_shake(delta: float) -> void: 
+	if shake_strength > 0:
+		shake_strength = lerpf(shake_strength,0,shakeFade * delta)
+		
+		offset = randomOffset()
