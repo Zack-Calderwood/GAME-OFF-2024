88b9e4a476b28aff3ed8ab74d483d4db3a3fbfb2
Eriki Enos <Erikjenos@hotmail.com>
2024-11-12 16:24:36 -0500

    foot steps are now not visable

--- game-off-2024/Scripts/foot_step.gd
+++ game-off-2024/Scripts/foot_step.gd
@@ -15,8 +15,18 @@ func _process(delta: float) -> void:
 	var alpha = clamp(1.0 - (timer / timer_Max), 0.0, 1.0)
 	
 	modulate.a = alpha
-	
-	"if fade_timer >= fade_duration:"
-		
-	
+
+	if timer <= 0 :
+		self.get_parent().queue_free()
+
 	pass
+
+
+func flashlight_area_entered(area: Area2D) -> void:
+	self.visible = true 
+	pass # Replace with function body.
+
+
+func flashlight_area_exited(area: Area2D) -> void:
+	self.visible = false 
+	pass # Replace with function body.
