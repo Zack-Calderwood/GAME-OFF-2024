7a9fa29eeba3623996133d88e10d303c7d20fc0f
Zack-Calderwood <zack.calderwood@hotmail.ca>
2024-11-28 18:50:09 -0500

    placeholder scripts
    will add more to the scripts later

--- game-off-2024/Scripts/coffin_puzzle.gd
+++ game-off-2024/Scripts/coffin_puzzle.gd
@@ -3,5 +3,5 @@ extends Area2D
 func _process(delta):
 	#WHen the plater presses e open up the puzzle menu
 	if Input.is_action_just_pressed("Use"):
-		get_tree().change_scene_to_file("res://Scenes/mainMenu.tscn")
+		get_tree().change_scene_to_file("res://Scenes/churchLVL.tscn")
 	
