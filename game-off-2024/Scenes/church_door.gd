1784b2c15b80829f82335a07f592406c915a5ef6
Eriki Enos <Erikjenos@hotmail.com>
2024-11-30 22:34:34 -0500

    ssss

--- game-off-2024/Scenes/church_door.gd
+++ game-off-2024/Scenes/church_door.gd
@@ -1,6 +1,6 @@
-extends CollisionShape2D
+extends Node2D
 
-
+@export var position_offset: Vector2 = Vector2.ZERO
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,3 +9,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("door")
+	
+	if body.name == "Player":
+		body.global_position = position_offset
+	pass # Replace with function body.
