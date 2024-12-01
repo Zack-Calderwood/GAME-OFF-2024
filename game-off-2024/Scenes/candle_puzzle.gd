d4771cca228dceb01b4c22ae1123b5a2f89a95bc
Eriki Enos <Erikjenos@hotmail.com>
2024-11-30 20:17:37 -0500

    level 3 puzzle

--- game-off-2024/Scenes/candle_puzzle.gd
+++ game-off-2024/Scenes/candle_puzzle.gd
@@ -6,6 +6,7 @@ var symbol = ""
 @onready var cryptsPuzzle = $".."
 @onready var symbol_text = $symbolText
 @onready var note_text = $noteText
+@export var note = Node2D
 
 var lit = false
 @onready var light = $PointLight2D
@@ -18,13 +19,14 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	symbol_text.text = str(symbol)
-	note_text.text = "Pos: " + str(position_in_array) + " sym: " + str(symbol)
+	note.text = "Pos: " + str(position_in_array) + " sym: " + str(symbol)
 	pass
 
 
 func player_entered(area: Area2D) -> void:
-	cryptsPuzzle.add_to_array(self)
-	toggle_lit()
+	if area.name == "Player":
+		cryptsPuzzle.add_to_array(self)
+		toggle_lit()
 	pass # Replace with function body.
 	
 func toggle_lit()->void:
