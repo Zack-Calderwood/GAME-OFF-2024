b404aa875fd7904dadaaa85bc68e3a1d5a5fc9da
Eriki Enos <Erikjenos@hotmail.com>
2024-11-30 23:34:13 -0500

    dd

--- game-off-2024/Scripts/coffin_puzzle.gd
+++ game-off-2024/Scripts/coffin_puzzle.gd
@@ -1,9 +1,20 @@
 extends Area2D
 
+var boolToggle = false 
 
 
 func _process(delta):
 	#WHen the plater presses e open up the puzzle menu
-	if $"../..".score >= 3 :
+	if $"../..".score >= 3 && boolToggle:
 		if Input.is_action_just_pressed("Use"):
 			get_tree().change_scene_to_file("res://Scenes/churchLVL.tscn")
+
+
+func _on_body_entered(body: Node2D) -> void:
+	boolToggle = true 
+	pass # Replace with function body.
+
+
+func _on_body_exited(body: Node2D) -> void:
+	boolToggle = false 
+	pass # Replace with function body.
--- game-off-2024/Scripts/organ.gd
+++ game-off-2024/Scripts/organ.gd
@@ -1,6 +1,7 @@
-extends CollisionShape2D
+extends Node2D
+@export var game = Node2D
 
-
+var boolToggle = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,7 +9,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if $"../..".score >= 3 :
+	if game.score >= 3 && boolToggle:
 		if Input.is_action_just_pressed("Use"):
 			get_tree().change_scene_to_file("res://Scenes/cryptsLVL.tscn")
 	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("Player")
+	boolToggle = true
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	boolToggle = false
+	pass # Replace with function body.
